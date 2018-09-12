e5a22b6fa99577753973fb3510b7f92875fe6c53
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-12 18:04:28 +0900

    show todo

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@
 var vm = new  Vue({
     el: '#app',
     data: {
-        name: 'taguchi'
+        todos: [
+            'task1',
+            'task2',
+            'task3'
+        ] 
     }
 });
 })();
