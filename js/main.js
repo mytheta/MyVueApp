a0dccc1e383201d4e91289b58b2f046bac06856a
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-13 19:31:38 +0900

    [add] dataをlocalstrageに格納

--- js/main.js
+++ js/main.js
@@ -5,17 +5,21 @@ var vm = new  Vue({
     el: '#app',
     data: {
         newItem: '',
-        todos: [  {
-            title: 'task1',
-            isDone: false
-        } , {
-            title: 'task2',
-            isDone: false,
-        } , {
-            title: 'task3',
-            isDone: false,
-        } ]
+        todos: [ ]
     },
+    watch: {
+        todos: {
+            handler: function(){
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            },
+            deep: true
+
+        }
+    },
+    mounted: function() {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+
     methods: {
         addItem: function() {
             var item = {
